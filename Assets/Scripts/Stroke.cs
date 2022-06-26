c7a73061836e84c1bdd19060c1d68f62c730bf65
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-06-26 11:34:12 +0900

    クリックされたときの処理

--- Assets/Scripts/Stroke.cs
+++ Assets/Scripts/Stroke.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Stroke : MonoBehaviour
 {
@@ -25,6 +26,41 @@ public class Stroke : MonoBehaviour
     
     void Update()
     {
-        
+        // クリックをしたタイミング
+        if(Input.GetMouseButtonDown(0))
+        {
+            //lineObjを生成、初期化する
+            addLineObject();
+        }
+    }
+
+    void addLineObject()
+    {
+        //空のゲームオブジェクト作成
+        GameObject lineObj = new GameObject();
+        //オブジェクトの名前をStrokeに変更
+        lineObj.name = "Stroke";
+        //lineObjにLineRendereコンポーネント追加
+        lineObj.AddComponent<LineRenderer>();
+        //lineRendererリストにlineObjを追加
+        lineRenderers.Add(lineObj.GetComponent<LineRenderer>());
+        //lineObjを自身の子要素に設定
+        lineObj.transform.SetParent(transform);
+        //lineObj初期化処理
+        initRenderes();
+    }
+
+    //lineObj初期化処理
+    void initRenderes()
+    {
+        //線を繋ぐ点を0に初期化
+        lineRenderers.Last().positionCount = 0;
+        //マテリアルを初期化
+        lineRenderers.Last().material = lineMaterial;
+        //色の初期化
+        lineRenderers.Last().material.color = lineColor;
+        //太さの初期化
+        lineRenderers.Last().startWidth = lineWidth;
+        lineRenderers.Last().endWidth = lineWidth;
     }
 }
