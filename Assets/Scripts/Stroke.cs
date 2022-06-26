e3874ebe5bf12f5b4069939073e7f01feb17c886
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-06-26 14:11:13 +0900

    修正

--- Assets/Scripts/Stroke.cs
+++ Assets/Scripts/Stroke.cs
@@ -5,91 +5,86 @@ using System.Linq;
 
 public class Stroke : MonoBehaviour
 {
-    /// <summary>
-    /// 描く線のコンポーネントリスト
-    /// </summary>
-    private List<LineRenderer> lineRendererList;
+    ///<summary>
+    ///描く線のコンポーネントリスト
+    ///</summary>
+    private List<LineRenderer> lineRenderersList;
 
-    /// <summary>
-    /// 描く線のマテリアル
-    /// </summary>
+    ///<summary>
+    ///描く線のマテリアル
+    ///</summary>
     public Material lineMaterial;
 
-    /// <summary>
-    /// 描く線の色
-    /// </summary>
+    ///<summary>
+    ///描く線の色
+    ///</summary>
     public Color lineColor;
 
-    /// <summary>
-    /// 描く線の太さ
-    /// </summary>
+    ///<summary>
+    ///描く線の太さ
+    ///</summary>
     [Range(0, 10)] public float lineWidth;
 
-
-    void Awake()
+    private void Awake()
     {
-        lineRendererList = new List<LineRenderer>();
+        lineRenderersList = new List<LineRenderer>();
     }
 
-    void Update()
+    private void Update()
     {
-
-        // ボタンが押された時に線オブジェクトの追加を行う
+        //ボタンが押された時に線オブジェクトの追加を行う
         if (Input.GetMouseButtonDown(0))
         {
-            this.AddLineObject();
+            AddLineObject();
         }
-
-        // ボタンが押されている時、LineRendererに位置データの設定を指定していく
+        //ボタンが押されている時に,LineRendererに位置データの設定を指定していく
         if (Input.GetMouseButton(0))
         {
-            this.AddPositionDataToLineRendererList();
+            AddPositionDataToLineRendererList();
         }
     }
 
-    /// <summary>
-    /// 線オブジェクトの追加を行うメソッド
-    /// </summary>
+    ///<summary>
+    ///線オブジェクトの追加を行うメソッド
+    ///</summary>
     private void AddLineObject()
     {
-
-        // 追加するオブジェクトをインスタンス
+        //追加するオブジェクトをインスタンス
         GameObject lineObject = new GameObject();
 
-        // オブジェクトにLineRendererを取り付ける
+        //オブジェクトにLineRenderを取り付ける
         lineObject.AddComponent<LineRenderer>();
 
-        // 描く線のコンポーネントリストに追加する
-        lineRendererList.Add(lineObject.GetComponent<LineRenderer>());
+        //描く線のコンポーネントリストに追加する
+        lineRenderersList.Add(lineObject.GetComponent<LineRenderer>());
 
-        // 線と線をつなぐ点の数を0に初期化
-        lineRendererList.Last().positionCount = 0;
+        //線と線を繋ぐ点の数を0に初期化
+        lineRenderersList.Last().positionCount = 0;
 
-        // マテリアルを初期化
-        lineRendererList.Last().material = this.lineMaterial;
+        //マテリアルを初期化
+        lineRenderersList.Last().material = lineMaterial;
 
-        // 線の色を初期化
-        lineRendererList.Last().material.color = this.lineColor;
+       //線の色を初期化
+       lineRenderersList.Last().material.color = lineColor;
 
-        // 線の太さを初期化
-        lineRendererList.Last().startWidth = this.lineWidth;
-        lineRendererList.Last().endWidth = this.lineWidth;
+        //線の太さを初期化
+        lineRenderersList.Last().startWidth = lineWidth;
+        lineRenderersList.Last().endWidth   = lineWidth;
     }
 
-    /// <summary>
-    /// 描く線のコンポーネントリストに位置情報を登録していく
-    /// </summary>
+    ///<summary>
+    ///描く線のコンポーネントに位置情報を登録していく
+    ///</summary>
     private void AddPositionDataToLineRendererList()
     {
-
-        // 座標の変換を行いマウス位置を取得
+        //座標の変換を行いマウス位置を取得
         Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 1.0f);
         var mousePosition = Camera.main.ScreenToWorldPoint(screenPosition);
 
-        // 線と線をつなぐ点の数を更新
-        lineRendererList.Last().positionCount += 1;
-
-        // 描く線のコンポーネントリストを更新
-        lineRendererList.Last().SetPosition(lineRendererList.Last().positionCount - 1, mousePosition);
+        //線と線を繋ぐ点の数を更新
+        lineRenderersList.Last().positionCount += 1;
+        //描く線のコンポーネントリストを更新
+        lineRenderersList.Last().SetPosition(lineRenderersList.Last().positionCount - 1, mousePosition);
+    
     }
 }
